828a841c0858f9bc14ca98c9dab5632f47bb619e
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-08-11 18:34:24 +0500

    Library updates: Shows current, planned, finished. Prints stats using the new function.

--- templates/library.php
+++ templates/library.php
@@ -12,37 +12,17 @@
 
 		<?php endif; ?>
 
-		<p><?php total_books() ?> overall; <?php books_read_since('1 year') ?> read in the last year; <?php books_read_since('1 month') ?> <?php printf(__('read in the last month. That\'s', NRTD));?> <?php average_books('month'); ?>.</p>
+		<p><?php print_book_stats() ?></p>
 
 		<?php library_search_form() ?>
 
-		<h2>Planned books (<?php echo total_books('unread', 0) ?>):</h2>
+		<h2>Currently Reading (<?php echo total_books('reading', 0) ?>):</h2>
 
-		<?php if( have_books('status=unread&num=-1') ) : ?>
-
-			<ul>
-
-			<?php while( have_books('status=unread&num=-1') ) : the_book(); ?>
-
-				<li><a href="<?php book_permalink() ?>"><?php book_title() ?></a> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a></li>
-
-			<?php endwhile; ?>
-
-			</ul>
-
-		<?php else : ?>
-
-			<p>None</p>
-
-		<?php endif; ?>
-
-		<h2>Current books (<?php echo total_books('reading', 0) ?>):</h2>
-
-		<?php if( have_books('status=reading&num=-1') ) : ?>
+		<?php if( have_books('status=reading&orderby=started&num=-1') ) : ?>
 
 			<ul>
 
-			<?php while( have_books('status=reading&num=-1') ) : the_book(); ?>
+			<?php while( have_books('status=reading&orderby=started&num=-1') ) : the_book(); ?>
 
 				<li>
 					<p><a href="<?php book_permalink() ?>"><img src="<?php book_image() ?>" alt="<?php book_title() ?>" /></a></p>
@@ -59,7 +39,27 @@
 
 		<?php endif; ?>
 
-		<h2>Recent books (<?php echo total_books('read', 0) ?>):</h2>
+		<h2>Planned books (<?php echo total_books('unread', 0) ?>):</h2>
+
+		<?php if( have_books('status=unread&num=-1') ) : ?>
+
+			<ul>
+
+			<?php while( have_books('status=unread&orderby=random&num=-1') ) : the_book(); ?>
+
+				<li><a href="<?php book_permalink() ?>"><?php book_title() ?></a> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a></li>
+
+			<?php endwhile; ?>
+
+			</ul>
+
+		<?php else : ?>
+
+			<p>None</p>
+
+		<?php endif; ?>
+
+		<h2>Finished Reading (<?php echo total_books('read', 0) ?>):</h2>
 
 		<?php if( have_books('status=read&orderby=finished&order=desc&num=-1') ) : ?>
 
