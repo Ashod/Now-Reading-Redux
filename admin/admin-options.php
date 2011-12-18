f09e61e005ce5cac24d0bb7257c4ece14ded5ca3
Ashod <ashodnakashian@yahoo.com>
2011-12-18 20:13:23 +0400

    Reorganized admin options page.

--- admin/admin-options.php
+++ admin/admin-options.php
@@ -52,6 +52,7 @@ function nr_options()
 	<div class="wrap">
 
 		<h2>Now Reading Redux</h2>
+		<i>Version: ' . NOW_READING_VERSION . '</i>
 	';
 
     echo '
@@ -65,22 +66,98 @@ function nr_options()
 		<table class="form-table" width="100%" cellspacing="2" cellpadding="5">
 		
 			<tr valign="top">
-				<th scope="row">' . __('Amazon Web Services Access Key ID', NRTD) . '</th>
+				<th scope="row"> <h4>Library Options</h4><hr /></th>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label for="books_per_page">' . __("Books per page", NRTD) . '</label></th>
 				<td>
-					<input type="text" size="50" name="AWSAccessKeyId" value="' . htmlentities($options['AWSAccessKeyId'], ENT_QUOTES, "UTF-8") . '" />
+					<input type="text" name="books_per_page" id="books_per_page" style="width:4em;" value="' . ( intval($options['booksPerPage']) ) . '" />
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">' . __("Use <code>mod_rewrite</code> enhanced library?", NRTD) . '</th>
+				<td>
+					<input type="checkbox" name="use_mod_rewrite" id="use_mod_rewrite"' . ( ($options['useModRewrite']) ? ' checked="checked"' : '' ) . ' />
 					<p>
-					' . sprintf(__("Required to add books from Amazon.  It's free to sign up. Register <a href='%s'>here</a>.", NRTD), "https://aws-portal.amazon.com/gp/aws/developer/registration/index.html") . '
+						' . __("If you have an Apache webserver with <code>mod_rewrite</code>, you can enable this option to have your library use prettier URLs. Compare:", NRTD) . '
+					</p>
+					<p>
+						<code>/index.php?now_reading_single=true&now_reading_author=albert-camus&now_reading_title=the-stranger</code>
+					</p>
+					<p>
+						<code>/library/albert-camus/the-stranger/</code>
+					</p>
+					<p>
+						' . sprintf(__("If you choose this option, be sure you have a custom permalink structure set up at your <a href='%s'>Options &rarr; Permalinks</a> page.", NRTD), 'options-permalink.php') . '
+					</p>
+					<p>
+					' . __("Permalink base:") . ' ' . htmlentities(get_option('home')) . '/
+					<input type="text" name="permalink_base" id="permalink_base" value="' . htmlentities($options['permalinkBase']) . '" /></p>
+				</td>
+			</tr>
+		
+			<tr valign="top">
+				<th scope="row"> <h4>Sidebar Widget Options</h4><hr /></th>
+			</tr>
+			<tr valign="top">
+				<th scope="row">' . __('Show images only', NRTD) . '</th>
+				<td>
+					<input type="checkbox" name="sidebar_images_only" id="sidebar_images_only"' . ( ($options['sidebarImagesOnly']) ? ' checked="checked"' : '' ) . ' />
+					<p>
+					' . __("When checked only images will be shown in the sidebar. Otherwise, titles and authors are shown as text.", NRTD) . '
 					</p>
 				</td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">' . __('Amazon Web Services Secret Access Key', NRTD) . '</th>
+				<th scope="row">' . __('Wishlist URL', NRTD) . ':</th>
 				<td>
-					<input type="text" size="50" name="SecretAccessKey" value="' . htmlentities($options['SecretAccessKey'], ENT_QUOTES, "UTF-8") . '" />
+					<input type="text" name="wishlist_url" size="75" value="' . htmlentities($options['wishlistUrl'], ENT_QUOTES, "UTF-8") . '" />
 					<p>
-					' . sprintf(__("Required to add books from Amazon.  Found at the same site as above. Register <a href='%s'>here</a>.", NRTD), "https://aws-portal.amazon.com/gp/aws/developer/registration/index.html") . '
+					' . __("An optional link shown at the bottom of the side bar as \"Buy me a gift!\" It is typically used to link to an Amazon wishlist page, but can be to any page.", NRTD) . '
+					</p>
+					<p>
+					' . __("Add 'http://' to make the URL absolute and not relative to the current page.", NRTD) . '
 					</p>
 				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label for="def_book_count">' . __("Default number of books queried", NRTD) . '</label></th>
+				<td>
+					<input type="text" name="def_book_count" id="def_book_count" style="width:4em;" value="' . ( intval($options['defBookCount']) ) . '" />
+				</td>
+			</tr>
+						
+			<tr valign="top">
+				<th scope="row"> <h4>Admin Options</h4><hr /></th>
+			</tr>
+			<tr valign="top">
+				<th scope="row">' . __("Multiuser mode", NRTD) . '</th>
+				<td>
+					<input type="checkbox" name="multiuser_mode" id="multiuser_mode"' . ( ($options['multiuserMode']) ? ' checked="checked"' : '' ) . ' />
+					<p>
+					' . __("If you have a multi-user blog, setting this option will enable you to specify which user is reading which book.", NRTD) . '
+					</p>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">' . __('Admin menu layout', NRTD) . ':</th>
+				<td>
+					<label for="menu_layout_single">' . __('Single', NRTD) . '</label>
+					<input type="radio" name="menu_layout" id="menu_layout_single" value="single"' . ( ( $options['menuLayout'] == NR_MENU_SINGLE ) ? ' checked="checked"' : '' ) . ' />
+					<br />
+					<label for="menu_layout_single">' . __('Multiple', NRTD) . '</label>
+					<input type="radio" name="menu_layout" id="menu_layout_single" value="multiple"' . ( ( $options['menuLayout'] == NR_MENU_MULTIPLE ) ? ' checked="checked"' : '' ) . ' />
+					<p>
+					' . __("When set to 'Single', Now Reading will add a top-level menu with submenus containing the 'Add a Book', 'Manage Books' and 'Options' screens.", NRTD) . '
+					</p>
+					<p>
+					' . __("When set to 'Multiple', Now Reading will insert those menus under 'Write', 'Manage' and 'Options' respectively.", NRTD) . '
+					</p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row"> <h4>Manage Page Options</h4><hr /></th>
 			</tr>
 			<tr valign="top">
 				<th scope="row">' . __('Date format string', NRTD) . '</th>
@@ -109,24 +186,25 @@ function nr_options()
 					</p>
 				</td>
 			</tr>
+			
 			<tr valign="top">
-				<th scope="row">' . __('Show Only Images in Sidebar', NRTD) . '</th>
+				<th scope="row"> <h4>Amazon Options</h4><hr /></th>
+			</tr>
+			<tr valign="top">
+				<th scope="row">' . __('Amazon Web Services Access Key ID', NRTD) . '</th>
 				<td>
-					<input type="checkbox" name="sidebar_images_only" id="sidebar_images_only"' . ( ($options['sidebarImagesOnly']) ? ' checked="checked"' : '' ) . ' />
+					<input type="text" size="50" name="AWSAccessKeyId" value="' . htmlentities($options['AWSAccessKeyId'], ENT_QUOTES, "UTF-8") . '" />
 					<p>
-					' . __("When checked only images will be shown in the sidebar. Otherwise, titles and authors are shown as text.", NRTD) . '
+					' . sprintf(__("Required to add books from Amazon.  It's free to sign up. Register <a href='%s'>here</a>.", NRTD), "https://aws-portal.amazon.com/gp/aws/developer/registration/index.html") . '
 					</p>
 				</td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">' . __('Wishlist URL', NRTD) . ':</th>
+				<th scope="row">' . __('Amazon Web Services Secret Access Key', NRTD) . '</th>
 				<td>
-					<input type="text" name="wishlist_url" value="' . htmlentities($options['wishlistUrl'], ENT_QUOTES, "UTF-8") . '" />
+					<input type="text" size="50" name="SecretAccessKey" value="' . htmlentities($options['SecretAccessKey'], ENT_QUOTES, "UTF-8") . '" />
 					<p>
-					' . __("An optional link shown at the bottom of the side bar as \"Buy me a gift!\" It is typically used to link to an Amazon wishlist page, but can be to any page.", NRTD) . '
-					</p>
-					<p>
-					' . __("Add 'http://' to make the URL absolute and not relative to the current page.", NRTD) . '
+					' . sprintf(__("Required to add books from Amazon.  Found at the same site as above. Register <a href='%s'>here</a>.", NRTD), "https://aws-portal.amazon.com/gp/aws/developer/registration/index.html") . '
 					</p>
 				</td>
 			</tr>
@@ -158,7 +236,6 @@ function nr_options()
     }
 
     echo '
-
 					</select>
 					<p>
 					' . __("If you choose to link to your book's product page on Amazon.com using the <code>book_url()</code> template tag, you can specify which country-specific Amazon site to link to. Now Reading will also use this domain when searching.", NRTD) . '
@@ -167,6 +244,10 @@ function nr_options()
 					' . __("NB: If you have country-specific books in your catalogue and then change your domain setting, some old links might stop working.", NRTD) . '
 					</p>
 				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row"> <h4>Search Options</h4><hr /></th>
 			</tr>
 			<tr valign="top">
 				<th scope="row">' . __('Image size to use', NRTD) . ':</th>
@@ -179,55 +260,6 @@ function nr_options()
 					<p>
 					' . __("NB: This change will only be applied to books you add from this point onwards.", NRTD) . '
 					</p>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row">' . __('Admin menu layout', NRTD) . ':</th>
-				<td>
-					<label for="menu_layout_single">' . __('Single', NRTD) . '</label>
-					<input type="radio" name="menu_layout" id="menu_layout_single" value="single"' . ( ( $options['menuLayout'] == NR_MENU_SINGLE ) ? ' checked="checked"' : '' ) . ' />
-					<br />
-					<label for="menu_layout_single">' . __('Multiple', NRTD) . '</label>
-					<input type="radio" name="menu_layout" id="menu_layout_single" value="multiple"' . ( ( $options['menuLayout'] == NR_MENU_MULTIPLE ) ? ' checked="checked"' : '' ) . ' />
-					<p>
-					' . __("When set to 'Single', Now Reading will add a top-level menu with submenus containing the 'Add a Book', 'Manage Books' and 'Options' screens.", NRTD) . '
-					</p>
-					<p>
-					' . __("When set to 'Multiple', Now Reading will insert those menus under 'Write', 'Manage' and 'Options' respectively.", NRTD) . '
-					</p>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><label for="books_per_page">' . __("Books per page", NRTD) . '</label></th>
-				<td>
-					<input type="text" name="books_per_page" id="books_per_page" style="width:4em;" value="' . ( intval($options['booksPerPage']) ) . '" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row"><label for="def_book_count">' . __("Default number of books displayed (used by the sidebar)", NRTD) . '</label></th>
-				<td>
-					<input type="text" name="def_book_count" id="def_book_count" style="width:4em;" value="' . ( intval($options['defBookCount']) ) . '" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row">' . __("Use <code>mod_rewrite</code> enhanced library?", NRTD) . '</th>
-				<td>
-					<input type="checkbox" name="use_mod_rewrite" id="use_mod_rewrite"' . ( ($options['useModRewrite']) ? ' checked="checked"' : '' ) . ' />
-					<p>
-						' . __("If you have an Apache webserver with <code>mod_rewrite</code>, you can enable this option to have your library use prettier URLs. Compare:", NRTD) . '
-					</p>
-					<p>
-						<code>/index.php?now_reading_single=true&now_reading_author=albert-camus&now_reading_title=the-stranger</code>
-					</p>
-					<p>
-						<code>/library/albert-camus/the-stranger/</code>
-					</p>
-					<p>
-						' . sprintf(__("If you choose this option, be sure you have a custom permalink structure set up at your <a href='%s'>Options &rarr; Permalinks</a> page.", NRTD), 'options-permalink.php') . '
-					</p>
-					<p>
-					' . __("Permalink base:") . ' ' . htmlentities(get_option('home')) . '/
-					<input type="text" name="permalink_base" id="permalink_base" value="' . htmlentities($options['permalinkBase']) . '" /></p>
 				</td>
 			</tr>
 			<tr valign="top">
@@ -245,7 +277,7 @@ function nr_options()
 			<tr valign="top">
 				<th scope="row">' . __("Proxy hostname and port", NRTD) . '</th>
 				<td>
-					<input type="text" name="proxy_host" id="proxy_host" value="' . $options['proxyHost'] . '" />:<input type="text" name="proxy_port" id="proxy_port" style="width:4em;" value="' . $options['proxyPort'] . '" />
+					<input type="text" name="proxy_host" id="proxy_host" size="50" value="' . $options['proxyHost'] . '" />:<input type="text" name="proxy_port" id="proxy_port" style="width:4em;" value="' . $options['proxyPort'] . '" />
 					<p>
 					' . __("Don't worry if you don't understand this; unless you're having problems searching for books, the default setting will be fine.", NRTD) . '
 					</p>
@@ -260,15 +292,7 @@ function nr_options()
 					</p>
 				</td>
 			</tr>
-			<tr valign="top">
-				<th scope="row">' . __("Multiuser mode", NRTD) . '</th>
-				<td>
-					<input type="checkbox" name="multiuser_mode" id="multiuser_mode"' . ( ($options['multiuserMode']) ? ' checked="checked"' : '' ) . ' />
-					<p>
-					' . __("If you have a multi-user blog, setting this option will enable you to specify which user is reading which book.", NRTD) . '
-					</p>
-				</td>
-			</tr>
+			
 		</table>
 
 		<input type="hidden" name="update" value="yes" />
