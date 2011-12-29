561dcd814675abd8936c0e9ad4ee6c490a0567d2
Ashod <ashodnakashian@yahoo.com>
2011-12-29 21:16:11 +0400

    Sidebar now uses new options and renders using shelf.php template to render each shelf.

---
+++ templates/shelf.php
@@ -0,0 +1,92 @@
+<?php
+
+	global $book_query, $library_options, $shelf_name, $shelf_option;
+	
+	//$book_count = total_books($book_status, false);
+	if ($shelf_option['viz'] != 'hide')
+	{
+?>
+		<h4><?php echo $shelf_option['title'] ?> (<?php echo total_books($shelf_name, 0) ?>)</h4>
+<?php
+		if (have_books($book_query))
+		{
+			if ($library_options['style'] != 'table')
+			{
+				// List
+?>
+				<ul>
+				<?php while(have_books($book_query)) : the_book(); ?>
+					<li>
+						<?php if ($shelf_option['viz'] != 'show_text') : ?>
+							<a href="<?php book_permalink() ?>">
+								<img src="<?php book_image() ?>" alt="<?php echo esc_attr(book_title(false)); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"/></a>
+						<?php endif; ?>
+						<?php if ($shelf_option['viz'] != 'show_image') : ?>
+							<br /><strong><a href="<?php book_permalink() ?>"><?php book_title() ?></a></strong> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a>
+						<?php endif; ?>
+					</li>
+				<?php endwhile; ?>
+				</ul>
+<?php
+			}
+			else
+			{
+				// Table
+?>
+				<table class='nr-shelf'>
+				<?php
+					$col_ctr = 0;
+					$books_per_row = (int)$library_options['itemsPerRow'];
+					if ($books_per_row <= 0)
+					{
+						$books_per_row = 1;
+					}
+					
+					for ($i = 0; $i < $books_per_row - 1; $i++)
+					{
+				?>
+						<col class='nr-shelf-slot'/>
+				<?php
+					}
+				?>
+						<col/>
+				<?php
+					$tr_closed = false;
+					while( have_books($book_query) ) {
+						the_book();
+						$col_ctr++;
+						if ($col_ctr%$books_per_row == 1) {
+							$tr_closed = false;
+				?>
+					<tr>
+				<?php
+						}
+				?>
+						<td>
+							<a href="<?php echo book_permalink(false); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"><img src="<?php echo book_image(false); ?>" alt="<?php echo book_title(false); ?> by <?php echo book_author(false); ?>" /></a>
+						</td>
+				<?php
+						if ($col_ctr%$books_per_row == 0) {// || $col_ctr == $book_count) {
+							$tr_closed = true;
+				?>
+					</tr>
+				<?php
+						}
+					}
+					if (!$tr_closed) {
+						echo "</tr>\n";
+					}
+				?>
+				</table>
+			<?php
+			}
+		}
+		else
+		{
+			// No books.
+		?>
+			<p>None</p>
+		<?php
+		}
+	}
+?>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,67 +1,58 @@
-
 <style type="text/css">
-<?php echo sidebar_css() ?>
+	<?php echo sidebar_css(); ?>
 </style>
+
+<?php
+	global $book_query, $library_options, $shelf_name, $shelf_option;
+	$options = get_option(NOW_READING_OPTIONS);
+	$library_options = $options['sidebarOptions'];
+?>
 
 <div class="now-reading nr_widget">
 
-	<div align=center><br /><b><a href="<?php library_url() ?>">View Full Library</a></b></div>
+	<div align=center>
+		<br /><b><a href="<?php library_url() ?>">View Full Library</a></b>
+	</div>
 
-	<h4>Current books:</h4>
-	<?php if (have_books('status=reading&orderby=random')) : ?>
-		<ul>
-		<?php while(have_books('status=reading&orderby=random')) : the_book(); ?>
-			<li>
-                <a href="<?php book_permalink() ?>">
-                    <img src="<?php book_image() ?>" alt="<?php echo esc_attr(book_title(false)); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"/></a>
-                <?php if (!sidebar_images_only()) : ?>
-						<br /><strong><a href="<?php book_permalink() ?>"><?php book_title() ?></a></strong> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a>
-            	<?php endif; ?>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-	<?php else : ?>
-		<p>None</p>
-	<?php endif; ?>
+	<div class="booklisting">
+	<?php
+		$shelf_name = 'reading';
+		$book_query = "status=reading&orderby=random";
+		$shelf_option = $options['sidebarOptions']['readingShelf'];
+		nr_load_template('shelf.php', false);
+	?>
+	</div>
 
-	<h4>Planned books:</h4>
-	<?php if (have_books('status=unread&orderby=random')) : ?>
-		<ul>
-		<?php while(have_books('status=unread&orderby=random')) : the_book(); ?>
-			<li>
-                <a href="<?php book_permalink() ?>">
-                    <img src="<?php book_image() ?>" alt="<?php echo esc_attr(book_title(false)); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"/></a>
-                <?php if (!sidebar_images_only()) : ?>
-						<br /><strong><a href="<?php book_permalink() ?>"><?php book_title() ?></a></strong> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a>
-            	<?php endif; ?>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-	<?php else : ?>
-		<p>None</p>
-	<?php endif; ?>
+	<div class="booklisting">
+	<?php
+		$shelf_name = 'unread';
+		$book_query = "status=unread&orderby=random";
+		$shelf_option = $options['sidebarOptions']['unreadShelf'];
+		nr_load_template('shelf.php', false);
+	?>
+	</div>
 
-	<h4>Recent books:</h4>
-	<?php if (have_books('status=read&orderby=finished&order=desc')) : ?>
-		<ul>
-		<?php while(have_books('status=read&orderby=finished&order=desc')) : the_book(); ?>
-			<li>
-                <a href="<?php book_permalink() ?>">
-                    <img src="<?php book_image() ?>" alt="<?php echo esc_attr(book_title(false)); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"/></a>
-                <?php if (!sidebar_images_only()) : ?>
-						<br /><strong><a href="<?php book_permalink() ?>"><?php book_title() ?></a></strong> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a>
-            	<?php endif; ?>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-	<?php else : ?>
-		<p>None</p>
-	<?php endif; ?>
+	<div class="booklisting">
+	<?php
+		$shelf_name = 'onhold';
+		$book_query = "status=onhold&orderby=random";
+		$shelf_option = $options['sidebarOptions']['onholdShelf'];
+		nr_load_template('shelf.php', false);
+	?>
+	</div>
+
+	<div class="booklisting">
+	<?php
+		$shelf_name = 'read';
+		$book_query = "status=read&orderby=finished&order=desc";
+		$shelf_option = $options['sidebarOptions']['readShelf'];
+		nr_load_template('shelf.php', false);
+	?>
+	</div>
     
-    <?php if (have_wishlist_url()) : ?>
-    	<div align=center><b><a href="<?php wishlist_url() ?>">Buy me a gift!</a></b></div>
+	<?php if (have_wishlist_url()) : ?>
+		<div align=center><b><a href="<?php wishlist_url() ?>">Buy me a gift!</a></b></div>
 	<?php endif; ?>
-
-    <div align=center><?php library_search_form() ?></div>	
-
+	
+	<div align=center><?php library_search_form(); ?></div>	
 </div>
