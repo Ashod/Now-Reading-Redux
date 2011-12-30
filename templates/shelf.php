579214c64d573998df8e89eed557061a5cb4ff6c
Ashod <ashodnakashian@yahoo.com>
2011-12-30 12:31:31 +0400

    Cleaned up self.php.

--- templates/shelf.php
+++ templates/shelf.php
@@ -1,15 +1,15 @@
 <?php
-
-	global $book_query, $library_options, $shelf_name, $shelf_option;
+	global $book_query, $library_options, $shelf_title, $shelf_option;
+	global $empty_shelf_message;
 	
 	//$book_count = total_books($book_status, false);
 	if ($shelf_option['viz'] != 'hide')
 	{
-?>
-		<h4><?php echo $shelf_option['title'] ?> (<?php echo total_books($shelf_name, 0) ?>)</h4>
-<?php
+		echo $shelf_title;
+
 		if (have_books($book_query))
 		{
+			echo '<div class="booklisting">';
 			if ($library_options['renderStyle'] != 'table')
 			{
 				// List
@@ -43,51 +43,57 @@
 						$books_per_row = 1;
 					}
 					
+					echo "<colgroup>\n";
 					for ($i = 0; $i < $books_per_row - 1; $i++)
 					{
-				?>
-						<col class='nr-shelf-slot'/>
-				<?php
+						$polarity = ($i % 2 == 0) ? 'odd' : 'even';
+						echo "<col class='nr-shelf-slot-{$polarity}'/>\n";
 					}
-				?>
-						<col/>
-				<?php
+
+					echo "</colgroup>\n";
+
 					$tr_closed = false;
-					while( have_books($book_query) ) {
+					while (have_books($book_query))
+					{
 						the_book();
 						$col_ctr++;
-						if ($col_ctr%$books_per_row == 1) {
+						if ($col_ctr%$books_per_row == 1)
+						{
 							$tr_closed = false;
-				?>
-					<tr>
-				<?php
+							echo "<tr>\n";
 						}
 				?>
 						<td>
-							<a href="<?php echo book_permalink(false); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"><img src="<?php echo book_image(false); ?>" alt="<?php echo book_title(false); ?> by <?php echo book_author(false); ?>" /></a>
+						<?php if ($shelf_option['viz'] != 'show_text') : ?>
+							<a href="<?php book_permalink() ?>">
+								<img src="<?php book_image() ?>" alt="<?php echo esc_attr(book_title(false)); ?>" title="<?php echo esc_attr(book_title(false)); ?> by <?php echo esc_attr(book_author(false)); ?>"/></a>
+						<?php endif; ?>
+						<?php if ($shelf_option['viz'] != 'show_image') : ?>
+							<br /><strong><a href="<?php book_permalink() ?>"><?php book_title() ?></a></strong> by <a href="<?php book_author_permalink() ?>"><?php book_author() ?></a>
+						<?php endif; ?>
 						</td>
 				<?php
-						if ($col_ctr%$books_per_row == 0) {// || $col_ctr == $book_count) {
+						if ($col_ctr%$books_per_row == 0)
+						{
 							$tr_closed = true;
-				?>
-					</tr>
-				<?php
+							echo "</tr>\n";
 						}
 					}
-					if (!$tr_closed) {
+					
+					if (!$tr_closed)
+					{
 						echo "</tr>\n";
 					}
-				?>
-				</table>
-			<?php
+					
+					echo "</table>\n";
 			}
+
+			echo "</div>\n";
 		}
 		else
 		{
 			// No books.
-		?>
-			<p>None</p>
-		<?php
+			echo '<p><b>' .isset($empty_shelf_message) ? $empty_shelf_message : 'None' . '</b></p>';
 		}
 	}
 ?>
