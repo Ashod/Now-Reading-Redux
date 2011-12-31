9f032b4139bb34f608001baf519e4645ac6df344
Ashod <ashodnakashian@yahoo.com>
2011-12-31 18:59:30 +0400

    Updated suffusion sidebar template.

--- suffusion/sidebar.php
+++ suffusion/sidebar.php
@@ -116,8 +116,15 @@ if ($suf_nr_wid_search_show == 'bottom')
 
 ?>
 
-<?php if (!empty(wishlist_url(false))) : ?>
-	<p><a href="<?php wishlist_url() ?>">Buy me a gift!</a></p>
+<?php if (have_wishlist_url()) : ?>
+	<div class="nr_wishlist"><b><a href="<?php wishlist_url() ?>">Buy me a gift!</a></b></div>
 <?php endif; ?>
+
+	<div style="display:none; text-align:center; font-size:120%; padding: 4px; text-shadow: 0 0 0.1em grey;" class="nr_ads">
+		<a href="http://wordpress.org/extend/plugins/now-reading-redux/" target="_blank" style="text-decoration: none">
+		<i>Now Reading</i>
+		<div style="font-weight:bold; font-family: arial; position:relative; top:-7px; left:42px; font-size:140%; color:#999; text-shadow: 0 0 0.1em #FFFFCC;">Redux</div>
+		</a>
+	</div>
 
 </div>
