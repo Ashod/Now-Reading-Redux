b3fd536088f082d76a4253ef7665bd5791bef333
Ashod Nakashian <ashodnakashian@yahoo.com>
2012-11-12 19:24:57 -0500

    Improved search template.

--- templates/search.php
+++ templates/search.php
@@ -1,52 +1,62 @@
 <?php
-	global $book_query, $library_options, $shelf_title, $shelf_option;
-	global $empty_shelf_message;
-	$options = get_option(NOW_READING_OPTIONS);
-	$search_options = $options['searchOptions'];
-?>
+/**
+ * Book search template for the Now Reading Redux plugin.
+ */
 
-<?php get_header(); ?>
+global $book_query, $library_options, $shelf_title, $shelf_option;
+global $empty_shelf_message;
+$options = get_option(NOW_READING_OPTIONS);
+$search_options = $options['searchOptions'];
+get_header();
+?>
 
 <style type="text/css">
 	<?php echo $search_options['css'] ?>
 </style>
 
-<div id="main-col">
-	<div id="container" class="now-reading nr_library">
-		<div id="content" role="main">
-			<div class="post fix nr-post">
-				<h1 class="post-title entry-title">Library</h1>
+<div id="primary">
+	<div id="content" role="main" class="narrowcolumn primary now-reading">
+
+		<article <?php post_class('post nr-post'); ?>>
+			<header class="post-header">
+				<h1 class="posttitle"><?php _e('Search Results for', 'now-reading-redux');?> <i><?php search_query(); ?></i>:</h1>
 			
 				<div class="bookdata fix">
-					
-					<?php if (can_now_reading_admin()) : ?>
-						<div class="manage">
-							<span class="icon">&nbsp;</span>
-							<p>Admin: &raquo; <a href="<?php manage_library_url() ?>">Manage Books</a></p>
-						</div>
-					<?php endif; ?>
-					
-					<p><a href="<?php library_url() ?>">&larr; Back to library</a></p>
+<?php
+if( can_now_reading_admin() ) {
+?>
+					<div class="manage">
+						<span class="icon">&nbsp;</span>
+						<a href="<?php manage_library_url(); ?>"><?php _e('Manage Books', 'suffusion');?></a>
+					</div>
+<?php
+}
+?>
+					<div class="library">
+						<span class="icon">&nbsp;</span>
+						<a href="<?php library_url(); ?>"><?php _e('Back to library', 'now-reading-redux');?></a>
+					</div>
 				</div>
+			</header>
 
-				<div class="entry-content">
-				<?php
-					library_search_form();
+			<div class="entry-content">
+<?php
+				library_search_form();
 
-					$shelf_title = "<h2>" . __("Search results for ") . "<i>" . search_query(false) . "</i></h2>";
-					$shelf_option = $search_options;
-					$book_query = "status=all&search={$GLOBALS['query']}&num=" . $shelf_option['maxItems'];
-					$empty_shelf_message = __("Sorry, but there were no search results for your query.");
-					nr_load_template('shelf.php', false);
-		
-				?>
-				</div><!-- .entry-content -->
-			</div><!-- .post -->
-		</div><!-- #content -->
-	</div><!-- #container -->
+				$shelf_title = "<h2></h2>";
+				$shelf_option = $search_options;
+				$book_query = "status=all&search=" . search_query(false) . "&num=" . $shelf_option['maxItems'];
+				$empty_shelf_message = __("Sorry, but there were no search results for your query.");
+				nr_load_template('shelf.php', false);
 
-	<?php get_sidebar() ?>
+?>
+			</div><!-- /.booklisting -->
+		</article><!-- /.nr-post -->
+	</div><!-- /#content -->
+</div><!-- /#main-col -->
 
-</div>
+<?php get_sidebar(); ?>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
+?>
